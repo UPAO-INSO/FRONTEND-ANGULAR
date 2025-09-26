3e19e641ba007684f9af5d0dca95c8e10e4342e5
Gerson Portal <gportallinares21@gmail.com>
2025-09-26 01:39:20 -0500

    fix: agree netlify.toml for deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import {
   provideHttpClient,
   withFetch,
