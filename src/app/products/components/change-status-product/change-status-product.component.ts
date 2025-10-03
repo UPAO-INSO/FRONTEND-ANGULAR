fd5ee25cc8b71c0e80e93ae7caf56e467648188e
Gerson Portal <gportallinares21@gmail.com>
2025-10-02 23:53:50 -0500

    feat: change available products

---
+++ src/app/products/components/change-status-product/change-status-product.component.ts
@@ -0,0 +1,77 @@
+import {
+  Component,
+  effect,
+  inject,
+  input,
+  output,
+  signal,
+} from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
+import { OrderCartService } from 'src/app/orders/services/order-cart.service';
+import { Table } from 'src/app/tables/interfaces/table.interface';
+import {
+  ProductType,
+  Product,
+  PartialProductUpdate,
+} from '../../interfaces/product.type';
+import { ProductService } from '../../services/product.service';
+import { OrderProductTabsComponent } from 'src/app/orders/components/register-order/order-products/order-product-tabs/order-product-tabs.component';
+import { OrderProductItemComponent } from 'src/app/orders/components/register-order/order-products/order-product-item/order-product-item.component';
+
+@Component({
+  selector: 'app-change-status-product',
+  imports: [OrderProductTabsComponent, OrderProductItemComponent],
+  templateUrl: './change-status-product.component.html',
+})
+export class ChangeStatusProductComponent {
+  private productService = inject(ProductService);
+
+  selectedTable = input<Table | null | undefined>(null);
+  productTypes = input.required<ProductType[]>({});
+
+  refreshTrigger = input<number>(0);
+
+  refresh = output<void>();
+  closeModal = output<void>();
+
+  updateProductStaus = output<PartialProductUpdate>();
+
+  selectedCategoryId = signal<number | null>(null);
+
+  onProductStatusUpdated(product: PartialProductUpdate) {
+    this.updateProductStaus.emit(product);
+  }
+
+  constructor() {
+    effect(() => {
+      const trigger = this.refreshTrigger();
+      if (trigger > 0) {
+        this.onRefresh();
+      }
+    });
+  }
+
+  filteredProducts = rxResource({
+    params: () => ({
+      categoryId: this.selectedCategoryId(),
+      refreshTrigger: this.refreshTrigger(),
+    }),
+    stream: ({ params }) => {
+      if (!params.categoryId) return of([]);
+      return this.productService.fetchProductsByTypeId(params.categoryId);
+    },
+  });
+
+  onCloseModal() {
+    this.closeModal.emit();
+  }
+
+  onCategorySelected(category: ProductType) {
+    this.selectedCategoryId.set(category.id);
+  }
+
+  onRefresh() {
+    this.filteredProducts.reload();
+  }
+}
