1b928c3fd63416afa8c786b8d3ddd21182156dda
JhonTMich <jhontmichael@gmail.com>
2025-10-07 16:34:45 -0500

    feat: button for show-password in login

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,9 +4,9 @@ export const environment = {
   companyName2: 'App',
   companySlogan: 'Gestiona tus pedidos',
 
-  // API_URL: 'http://localhost:8080/api',
-  // WS_URL: '//localhost:8080',
-  API_URL: 'https://abominably-undeclared-dionne.ngrok-free.dev/api',
-  WS_URL: '//abominably-undeclared-dionne.ngrok-free.devt:8080',
+  API_URL: 'http://localhost:8080/api',
+  WS_URL: '//localhost:8080',
+  // API_URL: 'https://abominably-undeclared-dionne.ngrok-free.dev/api',
+  // WS_URL: '//abominably-undeclared-dionne.ngrok-free.devt:8080',
   // API_URL: 'https://pleasant-adventure-production.up.railway.app/api',
 };
